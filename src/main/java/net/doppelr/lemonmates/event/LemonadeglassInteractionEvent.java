b27305c290af62ca73fbc05ddffc6a8706991ce8
Doppel_R <doppelr1001@gmail.com>
2026-03-15 11:12:12 +0100

    planning logic stuff

--- src/main/java/net/doppelr/lemonmates/event/LemonadeglassInteractionEvent.java
+++ src/main/java/net/doppelr/lemonmates/event/LemonadeglassInteractionEvent.java
@@ -4,4 +4,19 @@ public class LemonadeglassInteractionEvent {
     // glass is clicked with decoration item - decoration is applied
     // glass is click-held with empty hand - lemonade is drank
     // glass is click-held with filled jug - lemonade is filled
+
+    // https://docs.neoforged.net/docs/items/interactions
+    //
+    // custom interaction logic
+    // if interaction
+    //      get target
+    //          if glass
+    //              if applicable item
+    //                  apply item to data if not present yet
+    //                  consume item once
+    //                  update model to reflect changes
+    //              if no item
+    //                  reduce lemonade by 1 level if possible
+    //                  update model
+    //              otherwise break
 }
